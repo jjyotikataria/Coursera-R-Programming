9fde736b125312f29d580edd85c3539f99014cfb
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-01-01 22:58:26 +0530

    Create rankall.R

---
+++ rankall.R
@@ -0,0 +1,6 @@
+rankall <- function(outcome, num = "best"){
+    
+    file <- read.csv("outcome-of-care-measures.csv", header = TRUE, colClasses = "character")
+    
+    data <- file[c(2,7,11,17,23)]
+}
