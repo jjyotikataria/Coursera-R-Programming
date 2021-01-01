df33000dbd49a7cf20e2d30c09146b02f6c8ff16
Jyoti Kataria <jyotikataria2008@gmail.com>
2021-01-01 23:04:28 +0530

    Update rankall.R

--- rankall.R
+++ rankall.R
@@ -3,4 +3,14 @@ rankall <- function(outcome, num = "best"){
     file <- read.csv("outcome-of-care-measures.csv", header = TRUE, colClasses = "character")
     
     data <- file[c(2,7,11,17,23)]
+    
+    if(!outcome %in% c("heart attack","heart failure","pneumonia")){
+        stop("Invalid outcome")
+    }
+    
+    
+    empty_df <- data.frame()
+    
+    
+    
 }
